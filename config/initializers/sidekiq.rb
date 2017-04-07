aa834381bc45428c717c17a513dfefda33bdf742
Yossi Shalem <yossi@getneema.com>
2017-04-07 11:15:59 +0300

    x

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
   end
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL']}
+    config.redis = { url: ENV['REDIS_URL'], size: 2}
   end
 
 end
