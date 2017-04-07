b97cb6584774810e40656c479f611e9ada4bc4a4
Yossi Shalem <yossi@getneema.com>
2017-04-07 11:06:29 +0300

    x

--- config/puma.rb
+++ config/puma.rb
@@ -15,6 +15,6 @@ on_worker_boot do
 
   # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
 
-  @sidekiq_pid ||= spawn('bundle exec sidekiq -c 2 -q default -q mailers')
+  @sidekiq_pid ||= spawn('bundle exec sidekiq -c 2 -e production -q default -q mailers')
   ActiveRecord::Base.establish_connection
 end
